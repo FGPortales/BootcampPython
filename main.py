3b7b8b9c390ba758ab3c65d869cf9e5728f481eb
FGPortales <fportalesar@gmail.com>
2021-02-04 07:22:47 -0500

    Using turtle

---
+++ main.py
@@ -0,0 +1,14 @@
+import another_module
+
+print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color('coral')
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
