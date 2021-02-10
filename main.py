3132446d7226ce4c5dc76260a0d43a6e682bdad0
FGPortales <fportalesar@gmail.com>
2021-02-09 23:17:01 -0500

    [IMP] prettytable

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import another_module
+# import another_module
+#
+# print(another_module.another_variable)
+#
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color('coral')
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-print(another_module.another_variable)
+from prettytable import PrettyTable
 
-from turtle import Turtle, Screen
-
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color('coral')
-timmy.forward(100)
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = 'l'
+print(table)
